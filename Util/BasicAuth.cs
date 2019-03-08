b038e192a719ad13ae5b7d104f25d2a424b99199
RFN2018 <44893745+RFN2018@users.noreply.github.com>
2019-03-08 11:43:00 +0000

    added

---
+++ Util/BasicAuth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+
+namespace SMSDeliveryNotifications.Util
+{
+    public class BasicAuth
+    {
+        public static bool Authenticate(HttpRequestHeaders headers)
+        {
+            List<string> credentials = new List<string>();
+            string cred = headers.Authorization.ToString();
+            bool authenticated = false;
+
+            string encodedUsernamePassword = cred.Substring("Basic ".Length).Trim();
+
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+            string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+            int seperatorIndex = usernamePassword.IndexOf(':');
+
+            credentials.Add(usernamePassword.Substring(0, seperatorIndex));
+            credentials.Add(usernamePassword.Substring(seperatorIndex + 1));
+
+            if(credentials[0] == ConfigurationManager.AppSettings["config:Username"] && 
+               credentials[1] == ConfigurationManager.AppSettings["config:Password"])
+            {
+                authenticated = true;
+            }
+
+
+            return authenticated;
+        }
+    }
+}
